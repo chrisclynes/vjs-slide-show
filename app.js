c1f5ee6b7c8e6c51c69d124b2971734a194f9282
Chris Clynes <chrisclynes@live.com>
2022-11-13 11:47:22 -0500

    completed js functionality, updated css to properly display overlay headings

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ prevBtn.addEventListener('click', () => {
 });
 
 function carousel() {
+    // slide position for carousel effect
+    if(counter === slides.length) counter = 0;
+    if(counter < 0) counter = slides.length - 1;
+
     slides.forEach(slide => {
         slide.style.transform = `translateX(-${counter * 100}%)`;
     })
